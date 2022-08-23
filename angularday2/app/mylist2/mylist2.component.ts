df3a4e0d7f189d79a45ad34cab88b0b6039278f1
ekjotsinghmakol <109962748+ekjotsinghmakol@users.noreply.github.com>
2022-08-23 17:01:53 +0530

    Add files via upload

---
+++ angularday2/app/mylist2/mylist2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mylist2',
+  templateUrl: './mylist2.component.html',
+  styleUrls: ['./mylist2.component.css']
+})
+export class Mylist2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
